006f5be17aeceaa8eae7cd059988cdf22fd7ae05
Tristan Lewis <tmlewis@mitre.org>
2014-10-17 23:27:31 -0400

    added SURF detection to see if this was working

--- code/imageProces.py
+++ code/imageProces.py
@@ -6,5 +6,9 @@ import config
 def process_image(image):
     if config.verbose:
         print 'running on image with edge algo'
-    edges = cv2.Canny(image, 100, 200)
+    #edges = cv2.Canny(image, 100, 200)
+    surf = cv2.SURF(400)
+    kp, des = surf.detectAndCompute(image,None)
+    edges = cv2.drawKeypoints(image,kp,None,(255,0,0),4)
+
     return edges
